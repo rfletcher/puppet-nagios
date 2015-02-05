d94208667ce70329aa09152e57e773143e542a48
Rick Fletcher <fletch@pobox.com>
2015-02-05 12:07:56 -0500

    Move some old files out of the way

---
+++ manifests/old/servicegroup.pp
@@ -0,0 +1,9 @@
+define nagios::servicegroup() {
+  $alias = inline_template('<%= name.capitalize -%>')
+
+  nagios_servicegroup { $name:
+    ensure => present,
+    alias  => $alias,
+    target => '/etc/nagios3/conf.d/servicegroups.cfg',
+  }
+}
